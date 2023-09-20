5eeea5cb4f903f35d7035b618fa9e3670a16011c
DinaraDervel <dinaradervel@gmail.com>
2023-09-20 11:17:16 -0600

    add colors to Tariff

--- src/components/Tariff/Tariff.jsx
+++ src/components/Tariff/Tariff.jsx
@@ -1,10 +1,28 @@
 import styles from "./Tariff.module.scss";
 
 const Tariff = (props) => {
+  let headerClass = styles.card__header;
+  let priceClass = styles.card__price;
+  if (props.info.color === "blue") {
+    headerClass += ` ` + styles.card_blue__header;
+    priceClass += ` ` + styles.card_blue__price;
+  }
+  if (props.info.color === "green") {
+    headerClass += ` ` + styles.card_green__header;
+    priceClass += ` ` + styles.card_green__price;
+  }
+  if (props.info.color === "red") {
+    headerClass += ` ` + styles.card_red__header;
+    priceClass += ` ` + styles.card_red__price;
+  }
+  if (props.info.color === "black") {
+    headerClass += ` ` + styles.card_black__header;
+    priceClass += ` ` + styles.card_black__price;
+  }
   return (
     <div className={styles.card}>
-      <div className={styles.card__header}>{props.info.name}</div>
-      <div className={styles.card__price}>
+      <div className={headerClass}>{props.info.name}</div>
+      <div className={priceClass}>
         <p>руб</p>
         <p>
           <span>{props.info.price}</span> /мес
